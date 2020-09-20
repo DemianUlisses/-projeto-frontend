b6e3e254a152afe7db86f32b501ece48ea27db8d
demian <demianubq@gmail.com>
2020-09-20 17:37:30 -0300

    Incluido o metodo set para incluir os valores da latitude e longitude

--- js/map.js
+++ js/map.js
@@ -7,12 +7,16 @@ var plong = document.getElementById('long');
         
         navigator.geolocation.getCurrentPosition(function(position) {
     
-           
+            var mySet = new Set();
+
             const latitude   = position.coords.latitude;
             const longitude  = position.coords.longitude;
             const coordinate = new google.maps.LatLng(latitude, longitude);
     
-            
+            mySet.add(latitude);
+            mySet.add(longitude);
+            mySet.add(coordinate);
+
             var optionsMap = {
                         center : coordinate,
                         zoom: 19,
@@ -28,8 +32,30 @@ var plong = document.getElementById('long');
                                 };
             var marker = new google.maps.Marker(configMarker);
 
+           
             plag.innerHTML  = latitude;
             plong.innerHTML = longitude;
     
         });
+
+        function init() {
+          var myLatlng = new google.maps.LatLng(34.04, -118.24);
+          var myOptions = {
+            zoom: 13,
+            center: myLatlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+          };
+      
+          var map = new google.maps.Map(document.getElementById('map'), myOptions);
+      
+        }
+      
+        google.maps.event.addDomListener(window, 'load', init);
+
+
+      
+
+
+
+        
     });
